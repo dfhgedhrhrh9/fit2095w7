86887270570cf997fdaca2acc989eaea80d871eb
Shengjie Su <vincent.su.1008@hotmail.com>
2020-09-18 01:11:46 +1000

    w7lab

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-let mongoose = require('mongoose');
-let bodyparser = require('body-parser');
-let express = require('express');
-
-let app = express();
-let teacherRouter= require ('./routers/teacherroute.js');
-let studentRouter = require('./routers/studentroute');
-
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const actors = require('./routers/actor');
+const movies = require('./routers/movie');
+const app = express();
 app.listen(8080);
-
-app.use(bodyparser.json());
-
-app.use('/teachers',teacherRouter);
-
-app.get('/students',studentRouter.getAllStudents);
-app.get('/students/:id',studentRouter.getStudentById);
-app.post('/students',studentRouter.insertStudent);
-app.delete('/students',studentRouter.deleteStudent);
-app.put('/students',studentRouter.updateStudent);
-
-mongoose.connect('mongodb://localhost:27017/week7db',{useNewUrlParser: true},function (err){
-    if (err) console.log(err);
-    else console.log('successful');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect('mongodb://localhost:27017/movies', function (err) {
+    if (err) {
+        return console.log('Mongoose - connection error:', err);
+    }
+    console.log('Connect Successfully');
 });
+//Configuring Endpoints
+//Actor RESTFul endpoionts 
+app.get('/actors', actors.getAll);
+app.post('/actors', actors.createOne);
+app.get('/actors/:id', actors.getOne);
+app.put('/actors/:id', actors.updateOne);
+app.post('/actors/:id/movies', actors.addMovie);
+app.delete('/actors/:id', actors.deleteOne);
+app.delete('/actors/:aid/:mid', actors.deleteOneMovie);
+app.delete('/actors/:id/movies', actors.deleteActorMovie);
+//Movie RESTFul  endpoints
+app.get('/movies', movies.getAll);
+app.post('/movies', movies.createOne);
+app.get('/movies/:id', movies.getOne);
+app.put('/movies/:id', movies.updateOne);
+app.delete('/movies/:id', movies.deleteOne);
+app.delete('/movies/:mid/:aid', movies.deleteOneMovie);
+app.post('/movies/:id/actors', movies.addActor);
+app.get('/movies/:year1/:year2',movies.getMany);
+app.delete('/movies',movies.deleteMany);
